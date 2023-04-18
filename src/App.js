86454cb5d43dfb00128598274187a4d4b95de81a
Daniele Basile <daniele.basile.job@gmail.com>
2023-04-18 10:22:48 +0200

    added reference to map container, and set useState hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
-
+import osm from "./osm-provider";
 import "./app.css";
 import "leaflet/dist/leaflet.css";
 import { Icon, divIcon, point } from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
+import { useRef, useState } from "react";
 function App() {
+  const [center, setCenter] = useState({ lat: 13.084622, lng: 80.248357 });
+  const ZOOM_LEVEL = 9;
+  const mapRef = useRef();
   const markers = [
     {
       geoCode: [48.86, 2.3522],
@@ -23,6 +27,8 @@ function App() {
     //iconUrl: "https://as2.ftcdn.net/v2/jpg/02/98/28/57/1000_F_298285715_ct4qtZOJH119A39TdMrbkLsfziVCX1Rz.jpg",
     iconUrl: require("./img/map.png"),
     iconSize: [38, 38],
+    iconAnchor: [17, 46],
+    popupAnchor: [3, -46],
   });
   const createCustomClusterIcon = (cluster) => {
     return new divIcon({
@@ -33,14 +39,18 @@ function App() {
   };
   return (
     <div>
-      <MapContainer center={[48.8566, 2.3522]} zoom={13}>
-        <TileLayer
+      <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
+        {/* <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <TileLayer
           attribution="Stamen Watercolor"
           url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
+        /> */}
+        <TileLayer
+          url={osm.maptiler.url}
+          attribution={osm.maptiler.attribution}
         />
         <MarkerClusterGroup
           chunkedLoading
